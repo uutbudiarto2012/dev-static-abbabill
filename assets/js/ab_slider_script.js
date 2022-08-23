fbe6a4854f2a1ebc1ec027305943fbe3162f069e
Feryanto <feryanto@Feryantos-MacBook-Pro.local>
2022-08-23 20:26:55 +0700

    update page live streaming

---
+++ assets/js/ab_slider_script.js
@@ -0,0 +1,103 @@
+const heroSlider = document.querySelector(".heroSlider")
+const categorySlider = document.querySelector(".categorySlider")
+const liveSlider = document.querySelector(".liveSlider")
+const cardProductSlide = document.querySelector(".cardProductSlide")
+if(heroSlider){
+    const swiper = new Swiper(".heroSlider", {
+        autoplay: {
+          delay: 3000,
+          disableOnInteraction: false,
+        },
+        speed: 1000,
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+      });
+}
+if(categorySlider){
+    const swiper = new Swiper(".categorySlider", {
+        slidesPerView: 5,
+        spaceBetween: 10,
+        breakpoints: {
+          640: {
+            slidesPerView: 4,
+            spaceBetween: 20,
+          },
+          992: {
+            slidesPerView: 6,
+            spaceBetween: 40,
+          },
+          1024: {
+            slidesPerView: 7,
+            spaceBetween: 10,
+          },
+          1200: {
+            slidesPerView: 10,
+            spaceBetween: 10,
+          },
+        },
+      });
+}
+
+if(liveSlider){
+    var swiper = new Swiper(".liveSlider", {
+        slidesPerView: 2,
+        spaceBetween: 10,
+        autoplay: {
+          delay: -1,
+          disableOnInteraction: false,
+        },
+        loop: true,
+        speed: 6000,
+
+        breakpoints: {
+          992: {
+            slidesPerView: 3,
+            spaceBetween: 10,
+          },
+          1024: {
+            slidesPerView: 4,
+            spaceBetween: 10,
+          },
+          1200: {
+            slidesPerView: 5,
+            spaceBetween: 10,
+          },
+        },
+      });
+}
+
+if(cardProductSlide){
+    var swiper = new Swiper(".cardProductSlide", {
+        slidesPerView: 2,
+        spaceBetween: 10,
+        hashNavigation: true,
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+        autoplay: {
+          delay: 1500,
+          disableOnInteraction: false,
+        },
+        grabCursor: true,
+        loop: true,
+        speed: 500,
+
+        breakpoints: {
+          992: {
+            slidesPerView: 4,
+            spaceBetween: 10,
+          },
+          1024: {
+            slidesPerView: 5,
+            spaceBetween: 10,
+          },
+          1200: {
+            slidesPerView: 6,
+            spaceBetween: 10,
+          },
+        },
+      });
+}
